d03d192769f01cf4b120f92e756b508c1f8d1ce2
NeyamulOmy <omy@eagle3dstreaming.com>
2025-11-07 17:52:09 +0600

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: process.env.BASEPATH,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/invoices',
+        permanent: true,
+        locale: false
+      }
+    ]
+  }
+}
+
+export default nextConfig
